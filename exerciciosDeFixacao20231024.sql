5b01a43e71538343e6e898b0322e86a5dec53239
Matheus <omatheusphilipe@gmail.com>
2023-10-24 21:52:05 -0300

    3º

--- exerciciosDeFixacao20231024.sql
+++ exerciciosDeFixacao20231024.sql
@@ -1,3 +1,5 @@
 CREATE TRIGGER after_insert AFTER INSERT ON Clientes FOR EACH ROW INSERT INTO Auditoria (mensagem) VALUES ('inserido em ');
  
- CREATE TRIGGER before_insert BEFORE DELETE ON Clientes FOR EACH ROW INSERT INTO Auditoria (mensagem) VALUES ('Tentativa de exclusão ');
+CREATE TRIGGER before_insert BEFORE DELETE ON Clientes FOR EACH ROW INSERT INTO Auditoria (mensagem) VALUES ('Tentativa de exclusão ');
+
+CREATE TRIGGER after_update AFTER UPDATE ON Clientes FOR EACH ROW INSERT INTO Auditoria (mensagem) VALUES (OLD.nome, NEW.nome);
